5800a041fd1c18edb71d8073d38dc5d1c12abf74
Julie-Des <julie.deshayes14@gmail.com>
2025-02-25 10:27:15 +0100

    First commit

---
+++ my-react-app/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import Navigation from "../components/Navigation"
+
+
+function About() {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+        </div>
+    )
+}
+
+export default About
