725e6218134b16522f716b9468d53389b14aeae0
Julie-Des <julie.deshayes14@gmail.com>
2025-03-04 09:43:38 +0100

    refactor: change name of component ApartmentSheet to CardsContainer

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import ApartmentSheet from "../components/ApartmentSheet"
+import CardsContainer from "../components/CardsContainer"
 import Banner from "../components/Banner"
 
 function Home() {
     return (
         <div>
             <Banner className="banner-home" title="Chez vous, partout et ailleurs"/>
-            <ApartmentSheet />
+            <CardsContainer />
         </div>
     )
 }
