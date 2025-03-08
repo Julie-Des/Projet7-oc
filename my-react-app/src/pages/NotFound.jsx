915a7c5c880ab9bacb2acc78bc19ba6f99e5148b
Julie-Des <julie.deshayes14@gmail.com>
2025-03-08 15:48:32 +0100

    feat: add responsive to NotFound page

--- my-react-app/src/pages/NotFound.jsx
+++ my-react-app/src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ function NotFound() {
     return (
         <div className="not-found">
             <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <p><span>Oups! La page que</span> vous demandez n'existe pas.</p>
             <Link to="/">Retourner sur la page d'accueil</Link>
         </div>
     )
